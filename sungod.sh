0ffb2b76e745a7669e8cdbc1b8c39c9fd080c541
Dayton Nolan <daytonn@gmail.com>
2014-08-23 01:14:57 -0500

    First pass

--- sungod.sh
+++ sungod.sh
@@ -2,6 +2,26 @@ if [ -z "$SUNGOD_PHEONIX_ROOT" ]; then
   SUNGOD_PHEONIX_ROOT="$HOME/Development/phoenix"
 fi
 
+function __sg_usage {
+  echo ""
+  echo "Usage:"
+  echo ""
+  echo "  Sungod runs common phoenix development tasks"
+  echo ""
+  echo "  sungod new [application] (mix pheonix.new [application] $PWD/[application] )"
+  echo "  sungod server (mix phoenix.server)"
+  echo "  sungod install (mix do deps.get, compile)"
+  echo "  sungod assets [-w] (gulp build, [-w gulp watch])"
+  echo ""
+  echo "  Or use the handy shortcut sg"
+  echo ""
+  echo "  sg n (new)"
+  echo "  sg s (server)"
+  echo "  sg i (install)"
+  echo "  sg a (assets)"
+  echo ""
+}
+
 function __sg_cdpheonix {
   cd "$SUNGOD_PHEONIX_ROOT"
 }
@@ -27,16 +47,57 @@ function __sg_new {
   __sg_server
 }
 
+function __sg_gulp_build {
+  gulp build
+}
+
+function __sg_gulp_watch {
+  gulp watch
+}
+
+function sungod {
+  sg "$@"
+}
+
 function sg {
-  if [[ "$1" == "new" ]] || [[ "$1" == "n" ]]; then
+  if [[ "$1" == "new" ]] ||
+     [[ "$1" == "n" ]]; then
     __sg_new "$@"
   fi
 
-  if [[ "$1" == "install" ]] || [[ "$1" == "i" ]]; then
+  if [[ "$1" == "install" ]] ||
+     [[ "$1" == "i" ]]; then
     __sg_install
   fi
 
-  if [[ "$1" == "server" ]] || [[ "$1" == "s" ]]; then
+  if [[ "$1" == "server" ]] ||
+     [[ "$1" == "s" ]]; then
     __sg_server
+  fi
+
+  if [[ "$1" == "server" ]] ||
+     [[ "$1" == "s" ]]; then
+    __sg_server
+  fi
+
+  if [[ "$1" == "assets" ]] ||
+     [[ "$1" == "a" ]] ||
+     [[ "$2" == "assets" ]] ||
+     [[ "$2" == "a" ]]; then
+
+    if [[ "$1" == "-w" ]] ||
+       [[ "$2" == "-w" ]]; then
+
+      __sg_gulp_watch
+
+    else
+
+      __sg_gulp_build
+
+    fi
+  fi
+
+  if [[ -z "$1" ]]; then
+    __sg_usage
   fi
 }
